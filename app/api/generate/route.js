e1589d1979857e580dc26fd26124b508a0d51c21
adeeelsameer <adilsameer99@gmail.com>
2024-08-18 15:11:11 +0530

    Solution for random input by user

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -6,13 +6,22 @@ You are a flashcard creator, you take in text and create multiple flashcards fro
 Both front and back should be one sentence long.
 Tailor the difficulty level of the flashcards to the user's specified.
 Use simple language to make the flashcards accessible to a wide range of users.
-If given a body of text, extract the most important and relevant information for the flasho
-You should return in the following JSON format:
+If given a body of text, extract the most important and relevant information for the flashcards.
+If it is something you cannot understand then return exatly one flashcard in the following JSON format:
 {
   "flashcards":[
     {
-      "front": "Front of the card",
-      "back": "Back of the card"
+      "front": "Your input cannot produce flashcards.",
+      "back": "Please try again with a different input."
+    }
+  ]
+}
+Or else you should return exactly 9 flashcards unless specified explicilty in the following JSON format:
+{
+  "flashcards":[
+    {
+      "front": str,
+      "back": str
     }
   ]
 }`
