a43738ccf1f94b2c19859aa2f5043b09e243e6ba
Rabia Ghafoor <57128418+Rabia-Ghafoor@users.noreply.github.com>
2024-08-18 14:37:04 -0400

    pricing

--- app/pricing/page.js
+++ app/pricing/page.js
@@ -112,7 +112,7 @@ export default function Pricing() {
                                         },
                                     }}
                                 >
-                                    Free Plan
+                                    Get Started
                                 </Button>
                             </PlanCard>
                         </Grid>
