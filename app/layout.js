df8ae91f279ac1969d4c91797478de16b50c26ae
Rabia Ghafoor <57128418+Rabia-Ghafoor@users.noreply.github.com>
2024-08-18 11:57:14 -0400

    enabled speed insights

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
