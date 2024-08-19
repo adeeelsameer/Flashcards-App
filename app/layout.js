0be3ef91dbb0d17771951942bf624a88bb23bc92
Rabia Ghafoor <57128418+Rabia-Ghafoor@users.noreply.github.com>
2024-08-19 02:32:01 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,7 @@ export default function RootLayout({ children }) {
         </head>
         <body className={inter.className}>
           {children} 
+        <Analytics />
         </body>
       </html>
     </ClerkProvider>
