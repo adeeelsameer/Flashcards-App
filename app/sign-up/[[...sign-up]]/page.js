d1822b2357abe8f7bdf665519fc3672ff1d24804
adeeelsameer <adilsameer99@gmail.com>
2024-08-20 17:29:27 +0300

    added porper margin to sign up

--- app/sign-up/[[...sign-up]]/page.js
+++ app/sign-up/[[...sign-up]]/page.js
@@ -1,37 +1,39 @@
 import React from 'react'
-import { Container, Box, Typography, AppBar, Toolbar, Button } from '@mui/material'
-import { SignIn, SignUp } from '@clerk/nextjs'
-import Link from 'next/link'
+import { Container, Box, Typography, Toolbar } from '@mui/material'
+import { SignUp } from '@clerk/nextjs'
 import ResponsiveAppBar from '@/app/components/Appbarsignup'
 
 export default function SignUpPage() {
-  return <Container
-    maxWidth="100vw"
-    disableGutters
-    sx={{
-      height: '100vh',
-      display: 'flex',
-      flexDirection: 'column',
-    }}
-  >
-    <ResponsiveAppBar />
-    <Box
+  return (
+    <Container
+      maxWidth="100vw"
+      disableGutters
       sx={{
+        height: '100vh',
         display: 'flex',
         flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        minHeight: 'calc(100vh - 64px)',
-        px: 2,
-        mt: { xs: '160px', md: '110px' },
       }}
     >
-      <Typography variant="h4" component="h1" gutterBottom>
-        Sign Up
-      </Typography>
-      <Box sx={{ width: '100%', maxWidth: '400px', mt: 1 }}>
-        <SignUp />
+      <ResponsiveAppBar />
+      <Toolbar />
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexGrow: 1,
+          px: 2,
+          mt: '62px'
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          Sign Up
+        </Typography>
+        <Box sx={{ width: '100%', maxWidth: '400px', mt: 1 }}>
+          <SignUp />
+        </Box>
       </Box>
-    </Box>
-  </Container>
+    </Container>
+  )
 }
